b9e11edc8c72b7256503ed5e1eed51a83812b6ee
thecatemma <hemasn@icloud.com>
2020-05-26 00:00:02 +0200

    Protect Data Model/Article

--- Model/Article.php
+++ Model/Article.php
@@ -2,5 +2,12 @@
 
 class Article 
 {
+    protected $_id;
+    protected $_title;
+    protected $_autor;
+    protected $_content;
+    protected $_dateCreation;
+    protected $_dateModification;
+    protected $_comments;
 
 }
