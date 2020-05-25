7fdf174633749568ddc11c36491c432375954379
thecatemma <hemasn@icloud.com>
2020-05-25 23:42:16 +0200

    Model /Add to Model

--- Model/Model.php
+++ Model/Model.php
@@ -5,13 +5,19 @@
 
 abstract class Modele
 {
+
+    //private static $_bdd;
+    //private static function setBdd()
+
     protected $_db;
 
     public function __construct()
     {
-        $db = new PDO('mysql:host=localhost:8889;dbname=blogoc;charset=utf8', 'root', 'root');
+        $db = new PDO('mysql:host=localhost:8889;dbname=blog;charset=utf8', 'root', 'root');
 
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
         $this->_db = $db;
     }
+
+    //ERRMODE = presise mistakes
 }
