cec42cc9eae1c5d4c2c36760078dbb134b0f28d5
thecatemma <hemasn@icloud.com>
2020-05-26 14:28:01 +0200

    Article --> function hydrate

--- Model/Article.php
+++ Model/Article.php
@@ -15,4 +15,17 @@ class Article
         $this->hydrate($data)
     }
 
+    public function hydrate(array $data)
+    {
+      foreach ($data as $key => $value)
+      {
+        $method = 'set'.ucfirst($key);
+
+        if (method_exists($this, $method))
+        {
+          $this->$method($value);
+        }
+      }
+    }
+
 }
