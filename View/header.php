130a444d585130b656695228ae11ffbcd6fcbfea
thecatemma <hemasn@icloud.com>
2020-05-28 18:42:31 +0200

    Header

---
+++ View/header.php
@@ -0,0 +1,8 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+	<head>
+<!-- Charset -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Billet simple pour l\'Alaska">
