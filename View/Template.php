31365f535938599f109e47d70fb1df1949521deb
thecatemma <hemasn@icloud.com>
2020-06-03 21:09:47 +0200

    add Template

---
+++ View/Template.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+
+<!--Template inclus dans chaque vue qui regroupe les éléments html communs à toutes les pages -->
+
+<html>	
+	<body>		
+	</body>
+</html>
---
+++ View/View.php
@@ -0,0 +1,2 @@
+//View: affiche les informations dans une page html
+//cette class permet de créer un objet, qui contient des variables
