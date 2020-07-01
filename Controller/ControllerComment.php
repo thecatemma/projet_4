2baaaa064917a866cddd01c8148d4adfd00c3f58
thecatemma <hemasn@icloud.com>
2020-07-01 09:48:48 +0200

    Controller Comment

--- Controller/ControllerComment.php
+++ Controller/ControllerComment.php
@@ -1,2 +1,11 @@
 <?php
-//Controleur qui gère les commentaires
+// Controleur qui gère les actions relatives aux commentaires
+class ControleurCommentaire {
+    private $_commentaire;
+
+    public function __construct() {
+        $this->_commentaire = new CommentaireManager;
+    }
+
+
+}
